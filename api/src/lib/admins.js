7d80ed150b2054a91d8d649383c506e3bb214915
oliver fu <ofu997@gmail.com>
2021-07-07 23:52:34 -0700

    define admins, add functions (login, createAdmin)

---
+++ api/src/lib/admins.js
@@ -0,0 +1,8 @@
+const admins = [
+  "ricardo.alvarezpimentel@yale.edu",
+  "ofu997@gmail.com",
+  "nathanellstrand@gmail.com",
+  "admin@email.com"
+]
+
+export default admins
--- api/src/services/users/users.js
+++ api/src/services/users/users.js
@@ -1,9 +1,12 @@
 import { db } from 'src/lib/db'
 import { requireAuth } from 'src/lib/auth'
+import admins from 'src/lib/admins'
+import bcrypt from 'bcryptjs'
 
 // Used when the environment variable REDWOOD_SECURE_SERVICES=1
 export const beforeResolver = (rules) => {
-  rules.add(requireAuth)
+  // rules.add(requireAuth)
+  rules.skip()
 }
 
 export const users = () => {
@@ -16,9 +19,26 @@ export const user = ({ id }) => {
   })
 }
 
-export const createUser = ({ input }) => {
+// flow: create admins. login as admins. protect /admins/register page. create other users.
+// FE validate
+export const createUser = async ({ input }) => {
+  const password = await bcrypt.hash(input.password, 10);
+  const isAdmin = (admins.includes(input.email)) ? true : false;
+  const data = { ...input, password, isAdmin }
   return db.user.create({
-    data: input,
+    data,
+  })
+}
+
+export const createAdmin = async ({ input }) => {
+  const password = await bcrypt.hash(input.password, 10);
+  const isAdmin = (admins.includes(input.email)) ? true : false;
+  const data = { ...input, password, isAdmin }
+  if (!isAdmin) {
+    throw new Error('you can\'t do that')
+  }
+  return db.user.create({
+    data,
   })
 }
 
@@ -32,5 +52,31 @@ export const updateUser = ({ id, input }) => {
 export const deleteUser = ({ id }) => {
   return db.user.delete({
     where: { id },
+  })
+}
+
+export const loginUser = async ({ input }) => {
+  const user = await db.user.findUnique({
+    where: { email: input.email },
+  })
+  const passwordMatch = await bcrypt.compare(input.password, user.password)
+  if (!passwordMatch) {
+    throw new Error('Invalid Login')
+  }
+  const generateLocalSessionPassword = () => {
+    let length=26, result = "",
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[]\{}|;':,./<>?";
+
+    for (let i=0, n = charset.length; i < length; i++) {
+      result += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return result;
+  };
+  const localSessionPassword = generateLocalSessionPassword();
+  return db.user.update({
+    data: {
+      localSessionPassword,
+    },
+    where: { email: input.email }
   })
 }
