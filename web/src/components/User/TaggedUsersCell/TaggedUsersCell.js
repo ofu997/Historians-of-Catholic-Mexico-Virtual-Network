9048fb939e5f3a6c9cc43806587c392ab8421d31
oliver fu <ofu997@gmail.com>
2021-08-07 18:12:16 -0700

    add TaggedUsersCell folder

---
+++ web/src/components/User/TaggedUsersCell/TaggedUsersCell.js
@@ -0,0 +1,62 @@
+import Users from 'src/components/User/Users'
+import Spinner from 'react-bootstrap/Spinner'
+// export const beforeQuery = ({ tag }) => ({ variables: { tag } })
+
+export const QUERY = gql`
+  query TaggedUser($tag: String) {
+    users: findUsersByTag(tag: $tag) {
+      id
+      email
+      name
+      bio
+      location
+      university
+      credentials
+      status
+      profilePicUrl
+      linkAcademia
+      linkTwitter
+      linkLinkedIn
+      otherMedia
+      pub1
+      pub1desc
+      pub2
+      pub2desc
+      pub3
+      pub3desc
+      pub4
+      pub4desc
+      focusByTopic
+      focusByEra
+      tagChurchStateRels
+      tagCathGender
+      tagRightLeftWing
+      tagViolenceMilitancyMartyrdom
+      tagCathYouthStudentGroups
+      tagNationalism
+      tagMigrations
+      tagModernitySecSciences
+      tagPressLitIntelHist
+      tagMusArts
+      tagVisCulture
+      tagTransIntlHist
+      tagLocRegHist
+      tagOralHist
+    }
+  }
+`
+
+export const Loading = () =>
+  <div className='spinner cntr-h'>
+    <Spinner animation="border" variant="success" />
+  </div>
+
+export const Empty = () => `\u2715`
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ users }) => {
+  return <Users users={users} />
+}
